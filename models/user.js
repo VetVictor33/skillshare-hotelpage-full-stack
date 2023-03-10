1e56c1b113d837b6d774d2520ca30ed6afc96c61
Victor Feliciano <victorjfeliciano@gmail.com>
2023-03-10 10:48:04 -0300

    add user model, mongoose user schema

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: 'First name is required',
+        trim: true,
+        max: 33
+    },
+    surname: {
+        type: String,
+        required: 'Surname is required',
+        trim: true,
+        max: 33
+    },
+    email: {
+        type: String,
+        required: 'Email adress is required',
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: 'Password is required'
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
